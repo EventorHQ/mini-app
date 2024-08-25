66ef7cf822cf6b2a6a6522604416a838fdd511b9
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-08-25 10:30:38 +0300

    fix: add vite prefix to env vars

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = process.env.API_URL || "http://127.0.0.1:3000";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:3000";
 export const APP_URL =
-  process.env.MINI_API_URL || "https://t.me/eventor_tgbot/local";
+  import.meta.env.VITE_MINI_APP_URL || "https://t.me/eventor_tgbot/local";
 
 export const IOS_PLATFORMS = ["ios", "macos", "web"];
