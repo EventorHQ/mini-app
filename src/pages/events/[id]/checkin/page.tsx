b9476f0400f85c04c45d6f76b757df398dc254e2
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-09-16 04:31:38 +0300

    [2/N] feat: checkin

--- src/pages/events/[id]/checkin/page.tsx
+++ src/pages/events/[id]/checkin/page.tsx
@@ -1,10 +1,29 @@
 import { useCheckinMutation, useGetCheckinDataQuery } from "@/api/events";
 import { useNavigate } from "@/hooks/use-navigate";
 import { useTabbarActions } from "@/hooks/use-tabbar-actions";
-import { useBackButton } from "@telegram-apps/sdk-react";
-import { Cell, List, Section } from "@telegram-apps/telegram-ui";
+import { useBackButton, useHapticFeedback } from "@telegram-apps/sdk-react";
+import { Cell, List, Placeholder, Section } from "@telegram-apps/telegram-ui";
 import { useEffect } from "react";
+import { toast } from "sonner";
 import { useParams } from "wouter";
+
+const Form = ({ form }: { form: Record<string, string> }) => {
+  const formFields = Object.entries(form);
+  if (formFields.length === 0) {
+    return (
+      <Placeholder description="Посетитель не заполнил форму"></Placeholder>
+    );
+  }
+  return (
+    <Section header="Форма">
+      {Object.entries(form).map(([key, value]) => (
+        <Cell key={key} subhead={key}>
+          {value}
+        </Cell>
+      ))}
+    </Section>
+  );
+};
 
 const Content = ({
   eventId,
@@ -16,14 +35,24 @@ const Content = ({
   const { data } = useGetCheckinDataQuery(eventId, initDataRaw);
   const { setParams } = useTabbarActions();
   const { mutateAsync } = useCheckinMutation(+eventId);
+  const navigate = useNavigate();
+  const haptic = useHapticFeedback();
 
   useEffect(() => {
     if (data) {
+      const handleClick = () => {
+        mutateAsync(initDataRaw).then(() => {
+          haptic.notificationOccurred("success");
+          navigate(`/events/${eventId}/details`);
+          toast.success("Check-in успешно проведен");
+        });
+      };
+
       setParams({
         isVisible: true,
-        text: "Провести Check In",
+        text: "Провести Check-in",
         disabled: data.check_in_date !== null,
-        onClick: () => mutateAsync(initDataRaw),
+        onClick: handleClick,
       });
     }
   }, [data]);
@@ -31,13 +60,7 @@ const Content = ({
   if (data) {
     return (
       <List>
-        <Section header="Форма">
-          {Object.entries(data.form).map(([key, value]) => (
-            <Cell key={key} subhead={key}>
-              {value}
-            </Cell>
-          ))}
-        </Section>
+        <Form form={data.form} />
       </List>
     );
   }
