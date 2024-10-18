2d1eec17e7043d68ef46a40e0c08d2952389f2c4
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-10-18 18:45:29 +0300

    temp: remove lazy loading

--- src/app/routing/router.tsx
+++ src/app/routing/router.tsx
@@ -1,38 +1,54 @@
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 import { Router, Route, Switch, Redirect } from "wouter";
 
 import Tabbar from "./Tabbar";
 import Setup from "@/pages/setup";
 import { Toaster } from "@/components/ui/sonner";
+import EventsPage from "@/pages/events/page";
+import CreateEventPage from "@/pages/events/create/page";
+import EventPage from "@/pages/events/[id]/page";
+import EventDetailsPage from "@/pages/events/[id]/details/page";
+import EventRegisterPage from "@/pages/events/[id]/register/page";
+import EditEventPage from "@/pages/events/[id]/edit/page";
+import EventCheckinPage from "@/pages/events/[id]/checkin/page";
+import EventCheckedInPage from "@/pages/events/[id]/people/checkin/page";
+import EventVisitorsPage from "@/pages/events/[id]/people/visitors/page";
+import EventFeedbackPage from "@/pages/events/[id]/people/feedback/page";
+import OrgsPage from "@/pages/orgs/page";
+import OrgPage from "@/pages/orgs/[id]/page";
+import OrgInvitationsPage from "@/pages/orgs/[id]/invitations/page";
+import CreateOrgPage from "@/pages/orgs/create/page";
+import ProfilePage from "@/pages/profile/page";
+import InvitationPage from "@/pages/invitations/[id]/page";
 
-const EventsPage = lazy(() => import("@/pages/events/page"));
-const EventPage = lazy(() => import("@/pages/events/[id]/page"));
-const EventDetailsPage = lazy(() => import("@/pages/events/[id]/details/page"));
-const EditEventPage = lazy(() => import("@/pages/events/[id]/edit/page"));
-const EventCheckinPage = lazy(() => import("@/pages/events/[id]/checkin/page"));
-const EventRegisterPage = lazy(
-  () => import("@/pages/events/[id]/register/page"),
-);
-const EventCheckedInPage = lazy(
-  () => import("@/pages/events/[id]/people/checkin/page"),
-);
-const EventVisitorsPage = lazy(
-  () => import("@/pages/events/[id]/people/visitors/page"),
-);
-const EventFeedbackPage = lazy(
-  () => import("@/pages/events/[id]/people/feedback/page"),
-);
-const CreateEventPage = lazy(() => import("@/pages/events/create/page"));
+// const EventsPage = lazy(() => import("@/pages/events/page"));
+// const EventPage = lazy(() => import("@/pages/events/[id]/page"));
+// const EventDetailsPage = lazy(() => import("@/pages/events/[id]/details/page"));
+// const EditEventPage = lazy(() => import("@/pages/events/[id]/edit/page"));
+// const EventCheckinPage = lazy(() => import("@/pages/events/[id]/checkin/page"));
+// const EventRegisterPage = lazy(
+//   () => import("@/pages/events/[id]/register/page"),
+// );
+// const EventCheckedInPage = lazy(
+//   () => import("@/pages/events/[id]/people/checkin/page"),
+// );
+// const EventVisitorsPage = lazy(
+//   () => import("@/pages/events/[id]/people/visitors/page"),
+// );
+// const EventFeedbackPage = lazy(
+//   () => import("@/pages/events/[id]/people/feedback/page"),
+// );
+// const CreateEventPage = lazy(() => import("@/pages/events/create/page"));
 
-const OrgsPage = lazy(() => import("@/pages/orgs/page"));
-const OrgPage = lazy(() => import("@/pages/orgs/[id]/page"));
-const OrgInvitationsPage = lazy(
-  () => import("@/pages/orgs/[id]/invitations/page"),
-);
-const CreateOrgPage = lazy(() => import("@/pages/orgs/create/page"));
+// const OrgsPage = lazy(() => import("@/pages/orgs/page"));
+// const OrgPage = lazy(() => import("@/pages/orgs/[id]/page"));
+// const OrgInvitationsPage = lazy(
+//   () => import("@/pages/orgs/[id]/invitations/page"),
+// );
+// const CreateOrgPage = lazy(() => import("@/pages/orgs/create/page"));
 
-const ProfilePage = lazy(() => import("@/pages/profile/page"));
-const InvitationPage = lazy(() => import("@/pages/invitations/[id]/page"));
+// const ProfilePage = lazy(() => import("@/pages/profile/page"));
+// const InvitationPage = lazy(() => import("@/pages/invitations/[id]/page"));
 
 export default function AppRouter() {
   return (
